02345714bec68c50738b7bff0bddd0f3d6000a46
stella <stellavinmaris@gmail.com>
2019-05-09 12:56:38 +0300

    added login function

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,5 +1,9 @@
+import { AlertService } from './../_services/alert.service';
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
+import { User } from '../_model/user';
+import { AuthenticationService, UserService } from '../_services';
+import { first } from 'rxjs/operators';
 
 @Component({
   selector: 'app-login',
@@ -8,9 +12,25 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent implements OnInit {
 
+  public user: User = new User();
+  public errorMsg: Object;
+  public success: Object;
+  loading = false;
+  submitted = false;
+
+
   constructor(
-    private router: Router
-  ) { }
+    private route: ActivatedRoute,
+    private router: Router,
+    private authenticationService: AuthenticationService,
+    private userService: UserService,
+    private alertService: AlertService
+  ) {
+    // redirect to home if already logged in
+    if (this.authenticationService.currentUserValue) {
+      this.router.navigate(['/deposits']);
+  }
+  }
 
   ngOnInit() {
   }
@@ -21,6 +41,23 @@ export class LoginComponent implements OnInit {
   }
 
   login() {
+    this.submitted = true;
+        // stop here if form is invalid
+        if (this.user === undefined) {
+            return;
+        }
+
+        this.loading = true;
+        this.authenticationService.login(this.user.username, this.user.password)
+            .pipe(first())
+            .subscribe(
+                data => {
+                    this.router.navigate(['/deposits']);
+                },
+                error => {
+                    this.alertService.error(error);
+                    this.loading = false;
+                });
 
   }
 
